7226409c1b293254e9ad4148ec5709b21d969f75
Mads Kristensen <madskristensen@users.noreply.github.com>
2020-06-03 15:29:02 -0700

    Updates

--- src/Pages/Extension.cshtml.cs
+++ src/Pages/Extension.cshtml.cs
@@ -2,10 +2,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-using System;
-
-using WebEssentials.AspNetCore.OutputCaching;
-
 namespace VsixGallery.Pages
 {
 	public class ExtensionModel : PageModel
@@ -24,7 +20,6 @@ namespace VsixGallery.Pages
 			Package = _helper.GetPackage(id);
 
 			string folder = $"wwwroot/extensions/{Package.ID}";
-			HttpContext.EnableOutputCaching(TimeSpan.FromDays(30), fileDependencies: folder);
 		}
 	}
 }
